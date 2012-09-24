0608f40cf4b0d3e8c901154156b1fd360e6c5f3b
USER <USER@USER-PC>
2012-09-24 10:40:30 +0300

    merge all functional

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -1,24 +1,182 @@
 <?php
 
-/**
- * SiteController is the default controller to handle user requests.
- */
-class SiteController extends CController {
+class SiteController extends Controller {
 
-    
     /**
-     * Index action is the default action in a controller.
+     * Declares class-based actions.
+     */
+//	public function actions()
+//	{
+//		return array(
+//			// captcha action renders the CAPTCHA image displayed on the contact page
+//			'captcha'=>array(
+//				'class'=>'CCaptchaAction',
+//				'backColor'=>0xFFFFFF,
+//			),
+//		);
+//	}
+
+    public function filters() {
+        return array(
+            'accessControl',
+        );
+    }
+
+    /**
+     * Specifies the access control rules.
+     * This method is used by the 'accessControl' filter.
+     * @return array access control rules
+     */
+    public function accessRules() {
+        return array(
+            array('allow', // allow readers only access to the view file
+                'actions' => array('index', 'error', 'login', 'test', 'logout', 'registration'),
+                'users' => array('*')
+            ),
+            array('deny', // deny everybody else
+                'users' => array('*')
+            ),
+        );
+    }
+
+    /**
+     * This is the default 'index' action that is invoked
+     * when an action is not explicitly requested by users.
      */
     public function actionIndex() {
-       echo "Front not find";
+        echo 'User Name: <b>' . Yii::app()->user->name . '</b>';
+        echo '<br/>------------------------<br/>';
+        foreach (get_class_methods(__CLASS__) as $methods) {
+            if (strpos($methods, 'action') === 0 && $methods !== 'actions') {
+                echo CHtml::link(substr($methods, 6), array('site/' . substr($methods, 6)));
+                echo '<br/>';
+            }
+        }
+//            d(get_class_methods(__CLASS__));
+    }
+
+    public function actionTest() {
+        echo 'p0st';
+        d($_POST);
+        echo 'get';
+        d($_GET);
+        echo 'delete';
+        d($_DELETE);
+        echo 'put';
+        d($_PUT);
     }
 
     /**
      * This is the action to handle external exceptions.
-     * @return JsonSerializable 
-     * @todo нужен обработчик ошибок
      */
     public function actionError() {
+        if ($error = Yii::app()->errorHandler->error) {
+            if (Yii::app()->request->isAjaxRequest)
+                echo $error['message'];
+            else
+                $this->render('error', $error);
+        }
     }
 
+    /**
+     * Displays the login page
+     */
+    public function actionLogin() {
+        if (!Yii::app()->user->getIsGuest()) {
+            $this->redirect(array('/user/index'));
+        }
+        $this->layout = 'login';
+
+        $service = Yii::app()->request->getQuery('service');
+        if (isset($service)) {
+
+            $authIdentity = Yii::app()->eauth->getIdentity($service);
+//            $authIdentity->redirectUrl = Yii::app()->user->returnUrl;
+            $authIdentity->redirectUrl = $this->createUrl('/user/index');
+            $authIdentity->cancelUrl = $this->createAbsoluteUrl('site/login');
+
+            if ($authIdentity->authenticate()) {
+                $identity = new EAuthUserIdentity($authIdentity);
+
+                // successful authentication
+                if ($identity->authenticate()) {
+                    Yii::app()->user->login($identity);
+
+                    // special redirect with closing popup window
+                    $authIdentity->redirect();
+                } elseif ($identity->errorCode == EAuthUserIdentity::ERROR_USER_NOT_REGISTERED) {
+                    $reg = new RegistrationForm();
+                    $reg->registration($identity, $service);
+                    
+                    // special redirect with closing popup window
+                    $authIdentity->redirect();
+                } else {
+                    // close popup window and redirect to cancelUrl
+                    $authIdentity->cancel();
+                }
+            }
+
+            // Something went wrong, redirect to login page
+            $this->redirect(array('/site/login'));
+        }
+
+        $model = new LoginForm;
+
+        // if it is ajax validation request
+        if (Yii::app()->getRequest()->isAjaxRequest && Yii::app()->getRequest()->getParam('ajax') == 'formLogin') {
+            echo CActiveForm::validate($model);
+            Yii::app()->end();
+        }
+
+        // collect user input data
+        if (isset($_POST['LoginForm'])) {
+            $model->attributes = $_POST['LoginForm'];
+            // validate user input and redirect to the previous page if valid
+            if ($model->validate() && $model->login())
+//                            $this->redirect(Yii::app()->user->returnUrl);
+                $this->redirect('/user/index');
+        }
+//            d(Yii::app()->getRequest()->getPost());
+        // display the login form
+        
+        $regModel = new RegistrationForm();
+        $this->render('login', array('model' => $model, 'regModel' => $regModel));
+    }
+    
+    protected function vkLogin(){
+        $this->layout = 'popup';
+        $this->render('login/auth');
+    }
+
+    /**
+     * Logs out the current user and redirect to homepage.
+     */
+    public function actionLogout() {
+        Yii::app()->user->logout();
+        $this->redirect(Yii::app()->homeUrl);
+    }
+
+    public function actionRegistration() {
+        $model = new RegistrationForm;
+
+        // if it is ajax validation request
+        if (Yii::app()->getRequest()->isAjaxRequest && Yii::app()->getRequest()->getParam('ajax') == 'formReg') {
+            echo CActiveForm::validate($model);
+            Yii::app()->end();
+        }
+
+        // collect user input data
+        if (isset($_POST['RegistrationForm'])) {
+            $model->attributes = $_POST['RegistrationForm'];
+            // validate user input and redirect to the previous page if valid
+            if ($model->validate() && $model->registration()){
+                $this->redirect('/user/index');
+            }
+        }
+        $this->redirect('/site/login');
+    }
+    
+    public function actionSocialReg(){
+        
+    }
 }
--- protected/controllers/UserController.php
+++ protected/controllers/UserController.php
@@ -1,15 +1,102 @@
 <?php
 
-/**
- * SiteController is the default controller to handle user requests.
- */
-class UserController extends ApiController {
+class UserController extends Controller
+{
+	/**
+	 * Declares class-based actions.
+	 */
+//	public function actions()
+//	{
+//		return array(
+//			// captcha action renders the CAPTCHA image displayed on the contact page
+//			'captcha'=>array(
+//				'class'=>'CCaptchaAction',
+//				'backColor'=>0xFFFFFF,
+//			),
+//		);
+//	}
+    
+        public $layout = 'user';
+        public $title = 'User Controller Param(change in action)';
+        
+        public function filters()
+	{
+		return array(
+			'accessControl',
+		);
+	}
 
-    /**
-     * Index action is the default action in a controller.
-     */
-    public function actionIsAuth() {
-        Yii::app()->render->sendResponse('Welcome to migom API');
-    }
+	/**
+	 * Specifies the access control rules.
+	 * This method is used by the 'accessControl' filter.
+	 * @return array access control rules
+	 */
+	public function accessRules()
+	{
+		return array(
+                        array('allow', // allow readers only access to the view file
+                            'actions'=>array('index', 'edit'),
+                            'users' => array('*')
+                        ),
+                        array('deny',   // deny everybody else
+                            'users' => array('*')
+                        ),
+		);
+	}
 
+	/**
+	 * This is the default 'index' action that is invoked
+	 * when an action is not explicitly requested by users.
+	 */
+	public function actionIndex()
+	{
+            $id = Yii::app()->request->getParam('id', Yii::app()->user->id);
+            $model = $this->loadModel($id);
+            $this->render('profile', array('model' => $model));
+	}
+        
+        public function actionEdit()
+	{
+            if(Yii::app()->user->getIsGuest()){
+                throw new CHttpException(404,'The requested page does not exist.');
+            }
+            $id = Yii::app()->user->id;
+            $model = $this->loadModel($id);
+            $model->setScenario('general_update');
+            
+            if (Yii::app()->getRequest()->isAjaxRequest && Yii::app()->getRequest()->getParam('ajax') == 'generalForm') {
+                echo CActiveForm::validate($model);
+                Yii::app()->end();
+            }
+            
+            if (Yii::app()->getRequest()->isAjaxRequest && Yii::app()->getRequest()->getParam('ajax') == 'profileForm') {
+                $model->profile->setScenario('update');
+                echo CActiveForm::validate($model->profile);
+                Yii::app()->end();
+            }
+            
+            if($_POST['Profile']){
+                $model->profile = setScenario('update');
+                $model->profile->attributes = $_POST['Profile'];
+                if($model->profile->validate() && $model->profile->save())
+                    $this->redirect('/user/index');
+            }
+            
+            if($_POST['Users']){
+                $model->setScenario('general_update');
+                $model->attributes = $_POST['Users'];
+                if($model->validate() && $model->save())
+                    $this->redirect('/user/index');
+            }
+            
+            $this->render('edit', array('model' => $model));
+	}
+        
+        public function loadModel($id)
+	{
+		$model=Users::model()->findByPk($id);
+		if($model===null)
+			throw new CHttpException(404,'The requested page does not exist.');
+		return $model;
+	}
 }
