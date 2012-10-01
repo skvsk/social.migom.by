586a9161150b6d3c6e67ca5df91dec8e7a039a87
USER <USER@USER-PC>
2012-10-01 17:34:40 +0300

    gravatar

--- protected/services/UserService.php
+++ protected/services/UserService.php
@@ -20,6 +20,6 @@ class UserService {
     public static function uploadAvatarFromEmail($user_id, $email = null){
         $gravatarHash = (!empty($email))? $email:  rand(0, 99999999);
         return UserService::uploadAvatarFromService($user_id, 
-                                'http://www.gravatar.com/avatar/'. md5($gravatarHash) .'?f=y&d=identicon');
+                                'http://www.gravatar.com/avatar/'. md5($gravatarHash . ' ') .'?f=y&d=identicon');
     }
 }
