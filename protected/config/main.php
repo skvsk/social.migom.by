97c096eef01a1d2b8c24753cff4fd6d9d0b59ea7
USER <USER@USER-PC>
2012-10-10 10:58:49 +0300

    session marge

--- protected/config/main.php
+++ protected/config/main.php
@@ -2,145 +2,142 @@
 
 // uncomment the following to define a path alias
 // Yii::setPathOfAlias('local','path/to/local-folder');
-
 // This is the main Web application configuration. Any writable
 // CWebApplication properties can be configured here.
 return array(
-	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'Social Migom BY',
-
-	// preloading 'log' component
-	'preload'=>array('log'),
-        'defaultController' => 'site',
-
-	// autoloading model and component classes
-	'import'=>array(
-		'application.models.*',
+    'basePath' => dirname(__FILE__) . DIRECTORY_SEPARATOR . '..',
+    'name' => 'Social Migom BY',
+    // preloading 'log' component
+    'preload' => array('log'),
+    'defaultController' => 'site',
+    // autoloading model and component classes
+    'import' => array(
+        'application.models.*',
         'application.models.Api.*',
         'application.models.Mongo.*',
-		'application.components.*',
-		'application.extensions.RESTClient.*',
-		'application.extensions.yiidebugtb.*',
-		'application.services.*',
+        'application.components.*',
+        'application.extensions.RESTClient.*',
+        'application.extensions.yiidebugtb.*',
+        'application.services.*',
         'application.modules.api.components.*',
-		'ext.eoauth.*',
-		'ext.eoauth.lib.*',
-		'ext.lightopenid.*',
-		'ext.eauth.*',
-		'ext.eauth.custom_services.*',
+        'ext.eoauth.*',
+        'ext.eoauth.lib.*',
+        'ext.lightopenid.*',
+        'ext.eauth.*',
+        'ext.eauth.custom_services.*',
         'ext.YiiMongoDbSuite.*',
         'ext.YiiMongoDbSuite.extra.*',
-	),
-
-	'modules'=>array(
-		// uncomment the following to enable the Gii tool
-		'gii'=>array(
-			'class'=>'system.gii.GiiModule',
-			'password'=>'pass',
-			// If removed, Gii defaults to localhost only. Edit carefully to taste.
+    ),
+    'modules' => array(
+        // uncomment the following to enable the Gii tool
+        'gii' => array(
+            'class' => 'system.gii.GiiModule',
+            'password' => 'pass',
+        // If removed, Gii defaults to localhost only. Edit carefully to taste.
 			'ipFilters'=>array('86.57.245.247','::1'),
-		),
-                'api' => array(
-                    'keys' => array('devel'=>'86.57.245.247',
-                                    'test3migomby' => '178.172.181.139',
-                                    'test' => '127.0.0.1'
-                                    )
-                ),
-	),
-
-	// application components
-	'components'=>array(
-                'RESTClient' => array(
-                    'class' => 'application.extensions.RESTClient.RESTClient',
-                    'servers' => array(
-                        'migom' => array(
-                            'server' => 'http://test4.migom.by/api',
+        ),
+        'api' => array(
+            'keys' => array('devel' => '86.57.245.247',
+                'test3migomby' => '178.172.181.139',
+                'test' => '127.0.0.1'
+            )
+        ),
+    ),
+    // application components
+    'components' => array(
+        'RESTClient' => array(
+            'class' => 'application.extensions.RESTClient.RESTClient',
+            'servers' => array(
+                'migom' => array(
+                    'server' => 'http://test4.migom.by/api',
 //                            'http_auth' => true,
 //                            'http_user' => true,
 //                            'http_pass' => true,
-                            'key' => 'devel',
-                        ),
-                    ),
+                    'key' => 'devel',
                 ),
-            //todo вынести в модуль
-                'ApiException'=>array(
-                    'class'=>'ApiException',
-                    'status'=>200,
-                ),
-                'user'=>array(
-                    // enable cookie-based authentication
-                    'allowAutoLogin'=>true,
-                    'class' => 'WebUser',
-                    'loginUrl'=>array('site/login'),
-		),
-                'authManager' => array(
-                    // Будем использовать свой менеджер авторизации
-                    'class' => 'PhpAuthManager',
-                    // Роль по умолчанию. Все, кто не админы, модераторы и юзеры — гости.
-                    'defaultRoles' => array('guest'),
-                ),
-		// uncomment the following to enable URLs in path-format
-		'urlManager'=>array(
-			'urlFormat'=>'path',
-                        'showScriptName' => false,
-			'rules'=>array(
-                                'api' => 'api/default/index',
-                                'user/<id:\d+>'=>'user/index',
-                                'user'=>'user/index',
-                                '<action:\w+>'=>'site/<action>',
-                                '<controller:\w+>/<id:\d+>'=>'<controller>/view',
-                                '<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
-				'<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
-			),
-		),
-		// uncomment the following to use a MySQL database
-		'db'=>array(
-			'connectionString' => 'mysql:host=localhost;dbname=test4migomby',
-			'emulatePrepare' => true,
-			'username' => 'test4migomby',
-			'password' => 'ET7jS8zcoAKT',
-			'charset' => 'utf8',
-		),
-                'db2' => array(
-                    'class' => 'CDbConnection',
-                    'connectionString' => 'mysql:host=localhost;dbname=test',
-                    'username' => 'root',
-                    'password' => '',
-                    'charset' => 'utf8',
-                    'tablePrefix' => 'test.tbl_',
-                ),
- 
-                'mongodb' => array(
-                    'class'            => 'EMongoDB',
-                    'connectionString' => 'mongodb://localhost',
-                    'dbName'           => 'smigom',
-                    'fsyncFlag'        => false,
-                    'safeFlag'         => false,
-                    'useCursor'        => false              
-                ),
-            
-                'session' => array(
-                    'class' => 'CCacheHttpSession',
-                    'cacheID' => 'cache',
+            ),
+        ),
+        //todo вынести в модуль
+        'ApiException' => array(
+            'class' => 'ApiException',
+            'status' => 200,
+        ),
+        'user' => array(
+            // enable cookie-based authentication
+            'allowAutoLogin' => true,
+            'class' => 'WebUser',
+            'loginUrl' => array('site/login'),
+        ),
+        'authManager' => array(
+            // Будем использовать свой менеджер авторизации
+            'class' => 'PhpAuthManager',
+            // Роль по умолчанию. Все, кто не админы, модераторы и юзеры — гости.
+            'defaultRoles' => array('guest'),
+        ),
+        // uncomment the following to enable URLs in path-format
+        'urlManager' => array(
+            'urlFormat' => 'path',
+            'showScriptName' => false,
+            'rules' => array(
+//                'gii' => 'gii',
+//                'gii/<controller:\w+>' => 'gii/<controller>',
+//                'gii/<controller:\w+>/<action:\w+>' => 'gii/<controller>/<action>',
+                'api' => 'api/default/index',
+                'user/<id:\d+>' => 'user/index',
+                'user' => 'user/index',
+                '<action:\w+>' => 'site/<action>',
+                '<controller:\w+>/<id:\d+>' => '<controller>/view',
+                '<controller:\w+>/<action:\w+>' => '<controller>/<action>',
+                '<controller:\w+>/<action:\w+>/<id:\d+>' => '<controller>/<action>',
+            ),
+        ),
+        // uncomment the following to use a MySQL database
+        'db' => array(
+            'connectionString' => 'mysql:host=localhost;dbname=test4migomby',
+            'emulatePrepare' => true,
+            'username' => 'test4migomby',
+            'password' => 'ET7jS8zcoAKT',
+            'charset' => 'utf8',
+        ),
+        'db2' => array(
+            'class' => 'CDbConnection',
+            'connectionString' => 'mysql:host=localhost;dbname=test',
+            'username' => 'root',
+            'password' => '',
+            'charset' => 'utf8',
+            'tablePrefix' => 'test.tbl_',
+        ),
+        'mongodb' => array(
+            'class' => 'EMongoDB',
+            'connectionString' => 'mongodb://localhost',
+            'dbName' => 'smigom',
+            'fsyncFlag' => false,
+            'safeFlag' => false,
+            'useCursor' => false
+        ),
+        'session' => array(
+            'class' => 'CCacheHttpSession',
+            'cacheID' => 'cache',
 //                    'useCustomStorage' => true,
 //                    'keyPrefix' => 'miasdnb45'
 //                    'useCustomStorage' => true,
-                    'cookieParams'           => array('domain' => '.migom.by'),
-                ),
-                'cache'=>array(
-                    'class'=>'system.caching.CMemCache',
+            'cookieParams' => array('domain' => '.migom.by'),
+        ),
+        'cache' => array(
+            'class' => 'system.caching.CMemCache',
 //                    'useMemcached' => false,
-                    'servers'=>array(
-                        array(
-                            'host'=>'localhost',
-                            'port'=>11211,
-                        ),
-                    ),
+            'keyPrefix' => 'a1e7e8ff',
+            'servers' => array(
+                array(
+                    'host' => '178.172.181.139',
+                    'port' => 11211,
                 ),
-		'errorHandler'=>array(
-			// use 'site/error' action to display errors
-			'errorAction'=>'site/error',
-		),
+            ),
+        ),
+        'errorHandler' => array(
+            // use 'site/error' action to display errors
+            'errorAction' => 'site/error',
+        ),
 //		'log' => array(
 //                        'class' => 'CLogRouter',
 //                        'routes' => array(
@@ -156,15 +153,15 @@ return array(
 //                                ),
 //                        ),
 //                ),
-                'loid' => array(
-			'class' => 'ext.lightopenid.loid',
-		),
-		'eauth' => array(
-                    'class' => 'ext.eauth.EAuth',
-                    'popup' => true, // Use the popup window instead of redirecting.
-                    'cache' => false, // Cache component name or false to disable cache. Defaults to 'cache'.
-                    'cacheExpire' => 0, // Cache lifetime. Defaults to 0 - means unlimited.
-                    'services' => array( // You can change the providers and their classes.
+        'loid' => array(
+            'class' => 'ext.lightopenid.loid',
+        ),
+        'eauth' => array(
+            'class' => 'ext.eauth.EAuth',
+            'popup' => true, // Use the popup window instead of redirecting.
+            'cache' => false, // Cache component name or false to disable cache. Defaults to 'cache'.
+            'cacheExpire' => 0, // Cache lifetime. Defaults to 0 - means unlimited.
+            'services' => array(// You can change the providers and their classes.
 //                        'google' => array(
 //                            'class' => 'GoogleOpenIDService',
 //                        ),
@@ -177,13 +174,13 @@ return array(
 //                            'key' => '...',
 //                            'secret' => '...',
 //                        ),
-                        'google_oauth' => array(
-                            // register your app here: https://code.google.com/apis/console/
-                            'class' => 'CustomGoogleAuthService',
-                            'client_id' => '601138882389-tkfndj73f4cnnjpuu402ihva57ndscfb.apps.googleusercontent.com',
-                            'client_secret' => 'L_8-TQDdm31OEz9vXNfOWB8J',
-                            'title' => 'Google (OAuth)',
-                        ),
+                'google_oauth' => array(
+                    // register your app here: https://code.google.com/apis/console/
+                    'class' => 'CustomGoogleAuthService',
+                    'client_id' => '601138882389-tkfndj73f4cnnjpuu402ihva57ndscfb.apps.googleusercontent.com',
+                    'client_secret' => 'L_8-TQDdm31OEz9vXNfOWB8J',
+                    'title' => 'Google (OAuth)',
+                ),
 //                        'yandex_oauth' => array(
 //                            // register your app here: https://oauth.yandex.ru/client/my
 //                            'class' => 'YandexOAuthService',
@@ -191,12 +188,12 @@ return array(
 //                            'client_secret' => '...',
 //                            'title' => 'Yandex (OAuth)',
 //                        ),
-                        'facebook' => array(
-                            // register your app here: https://developers.facebook.com/apps/
-                            'class' => 'CustomFacebookOAuthService',
-                            'client_id' => '376588799076610',
-                            'client_secret' => 'e48917e90c261a4ec630b20abddbe8e0',
-                        ),
+                'facebook' => array(
+                    // register your app here: https://developers.facebook.com/apps/
+                    'class' => 'CustomFacebookOAuthService',
+                    'client_id' => '376588799076610',
+                    'client_secret' => 'e48917e90c261a4ec630b20abddbe8e0',
+                ),
 //                        'linkedin' => array(
 //                            // register your app here: https://www.linkedin.com/secure/developer
 //                            'class' => 'LinkedinOAuthService',
@@ -215,12 +212,12 @@ return array(
 //                            'client_id' => '...',
 //                            'client_secret' => '...',
 //                        ),
-                        'vkontakte' => array(
-                            // register your app here: https://vk.com/editapp?act=create&site=1
-                            'class' => 'CustomVKontakteOAuthService',
-                            'client_id' => '3142907',
-                            'client_secret' => '9b1FoGkG8u2Rtyi9mFC6',
-                        ),
+                'vkontakte' => array(
+                    // register your app here: https://vk.com/editapp?act=create&site=1
+                    'class' => 'CustomVKontakteOAuthService',
+                    'client_id' => '3142907',
+                    'client_secret' => '9b1FoGkG8u2Rtyi9mFC6',
+                ),
 //                        'mailru' => array(
 //                            // register your app here: http://api.mail.ru/sites/my/add
 //                            'class' => 'MailruOAuthService',
@@ -242,27 +239,26 @@ return array(
 //                            'client_secret' => '...',
 //                            'title' => 'Odnokl.',
 //                        ),
-                    ),
-                ),
-	),
-
-	// application-level parameters that can be accessed
-	// using Yii::app()->params['paramName']
+            ),
+        ),
+    ),
+    // application-level parameters that can be accessed
+    // using Yii::app()->params['paramName']
 //	'params'=>array(
 //		// this is used in contact page
 //		'adminEmail'=>'webmaster@example.com',
 //	),
-            'onBeginRequest' => function($event) {
-            $route = Yii::app()->getRequest()->getPathInfo();
-            $module = substr($route, 0, strpos($route, '/'));
+    'onBeginRequest' => function($event) {
+        $route = Yii::app()->getRequest()->getPathInfo();
+        $module = substr($route, 0, strpos($route, '/'));
 
-            if (Yii::app()->hasModule($module)) {
-                $module = Yii::app()->getModule($module);
-                if (isset($module->urlRules)) {
-                    $urlManager = Yii::app()->getUrlManager();
-                    $urlManager->addRules($module->urlRules);
-                }
+        if (Yii::app()->hasModule($module)) {
+            $module = Yii::app()->getModule($module);
+            if (isset($module->urlRules)) {
+                $urlManager = Yii::app()->getUrlManager();
+                $urlManager->addRules($module->urlRules);
             }
-            return true;
-        },
+        }
+        return true;
+    },
 );
