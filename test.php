bc8a911cab3641f608f1c57145d9968940e71aca
USER <USER@USER-PC>
2012-10-10 14:31:48 +0300

    session marge

--- test.php
+++ test.php
@@ -14,9 +14,16 @@ session_set_cookie_params(
 ); 
 
 session_start(); 
+echo 'session_id - ' . session_id();
 echo "<pre>";
 var_dump($_SESSION);
 //$_SESSION['test3'] = 'test32';
 var_dump($_SESSION);
 echo "</pre>";
+echo "memcache test <br/>";
+$memcache = new Memcache;
+$memcache->connect('178.172.181.139', 11211) or die ("Could not connect");
+//memcache_set($memcache, 'test', 'It is work', 0, 60*5);
+
+var_dump($memcache->get('test'));
 ?>
