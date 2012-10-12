880ce89d0c87ea22feff2c8788928d8982008d90
USER <USER@USER-PC>
2012-10-12 11:32:09 +0300

    refactoring

--- protected/models/comments/News.php
+++ protected/models/comments/News.php
@@ -19,7 +19,7 @@ class Comments_News extends Comments
         // class name for the relations automatically generated below.
         return array(
             'user' => array(self::BELONGS_TO, 'Users', 'user_id'),
-            'parent' => array(self::BELONGS_TO, 'NewsComments', 'parent_id'),
+            'parent' => array(self::BELONGS_TO, 'Comments_News', 'parent_id'),
         );
     }
 
