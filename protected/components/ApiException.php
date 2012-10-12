bbd758243ff78e311bd6ef61eb886a18a13696e5
USER <USER@USER-PC>
2012-10-12 13:54:53 +0300

    refactoring

--- protected/components/ApiException.php
+++ protected/components/ApiException.php
@@ -8,7 +8,7 @@ class ApiException extends CComponent {
      * @param string $message
      */
         public function __construct($message) {
-            Yii::app()->controller->module->render->sendResponse(array('message'=>$message));
+            Yii::app()->controller->module->render->sendResponse(array('message'=>$message, ApiComponent::CONTENT_SUCCESS => false));
             Yii::app()->end();
         }
 
