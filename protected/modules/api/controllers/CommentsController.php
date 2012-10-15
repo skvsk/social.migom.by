602a905cfa31e4b3158b231ce7311bd2cbad4795
schevgeny <schevgeny@gmail.com>
2012-10-15 18:06:45 +0300

    Update protected/modules/api/controllers/CommentsController.php
    dd

--- protected/modules/api/controllers/CommentsController.php
+++ protected/modules/api/controllers/CommentsController.php
@@ -129,7 +129,6 @@ class CommentsController extends ApiController
         $criteria->group='`t`.`entity_id`';
         $rawData = Comments::model($entity)->findAll($criteria);
         foreach ($rawData as $value) {
-dd($value);
             $res['id'] = $value->entity_id;
             $res['count'] = $value->cnt;
         }
