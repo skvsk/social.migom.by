4b734f302fc55db75563b08879baa1617833b938
Trueman <Trueman@192.168.1.128>
2012-10-15 12:01:50 +0300

    test VK API

--- vkApiTest.php
+++ vkApiTest.php
@@ -8,7 +8,7 @@ foreach($params as $key => $value){
     $string .= $key . '=' . $value;
 }
 
-curl_setopt($curl,CURLOPT_URL, 'https://api.vk.com/method/users.get');
+curl_setopt($curl,CURLOPT_URL, 'http://api.vk.com/api.php');
 curl_setopt($curl,CURLOPT_RETURNTRANSFER,true);
 curl_setopt($curl,CURLOPT_POST,true);
 curl_setopt($curl,CURLOPT_POSTFIELDS, array_merge($params, array('sig' => md5($string))));
