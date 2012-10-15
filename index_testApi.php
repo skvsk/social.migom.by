84c439788b793f7f470ade372f1e740c9521e72b
Trueman <Trueman@192.168.1.128>
2012-10-15 12:10:19 +0300

    test VK API

---
+++ index_testApi.php
@@ -0,0 +1,12 @@
+<?
+require 'vkapi.class.php';
+
+$api_id = 3142907; // Insert here id of your application
+$secret_key = '9b1FoGkG8u2Rtyi9mFC6'; // Insert here secret key of your application
+
+$VK = new vkapi($api_id, $secret_key);
+
+$resp = $VK->api('getProfiles', array('uids'=>'7314718'));
+
+print_r($resp);
+?>
