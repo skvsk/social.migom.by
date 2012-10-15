fa310eb648ec1127ffa179075efe9b546e34c671
USER <USER@USER-PC>
2012-10-15 11:42:25 +0300

    refactoring add files

---
+++ protected/models/mongo/likes/test/Comments.php
@@ -0,0 +1,17 @@
+<?php
+
+class Likes_Devel_Comments extends Likes
+{
+
+    /**
+     * This method have to be defined in every Model
+     * @return string MongoDB collection name, witch will be used to store documents of this model
+     */
+    public function getCollectionName()
+    {
+        return 'likes_test_comments';
+    }
+    
+    
+
+}
