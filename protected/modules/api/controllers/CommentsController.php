72280a1ebbc70c7abf96b95fe0187f21705620e3
USER <USER@USER-PC>
2012-10-15 12:07:08 +0300

    comment for user

--- protected/modules/api/controllers/CommentsController.php
+++ protected/modules/api/controllers/CommentsController.php
@@ -61,7 +61,6 @@ class CommentsController extends ApiController
         //TODO Как то не правельно related элименты так получать
         foreach ($rawData as $value) {
             $row = array();
-            d($value);
             foreach ($value as $key => $attr) {
                 $row[$key] = $attr;
             }
