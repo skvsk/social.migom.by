5123d299c5bc8cec6335b00d7afe2517e2c2f04a
USER <USER@USER-PC>
2012-10-16 15:00:26 +0300

    likes for nes comments

---
+++ protected/models/mongo/likes/migom/news/Comments.php
@@ -0,0 +1,23 @@
+<?php
+
+class Likes_Migom_News_Comments extends Likes
+{
+
+    /**
+     * This method have to be defined in every Model
+     * @return string MongoDB collection name, witch will be used to store documents of this model
+     */
+    public function getCollectionName()
+    {
+        return 'likes_migom_news_comments';
+    }
+
+    /**
+     * This method have to be defined in every model, like with normal CActiveRecord
+     */
+    public static function model($className = __CLASS__)
+    {
+        return parent::model($className);
+    }
+
+}
