b8e45bcb0d6643114b324a409ec88ba58e3dafcb
USER <USER@USER-PC>
2012-10-16 16:07:19 +0300

    likes for nes comments

--- protected/modules/api/controllers/LikesController.php
+++ protected/modules/api/controllers/LikesController.php
@@ -52,7 +52,7 @@ class LikesController extends ApiController
      * @param int $user_id
      * @access (is_int($id))
      */
-    public function actionPostLike($entity)
+    public function actionPostLike()
     {
         d($_POST);
         $res = $this->_likeUpdate($_POST['id'], $entity, 1);
